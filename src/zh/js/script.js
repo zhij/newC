18f11d9723a5503679ee58f93013fe0c6484b6ce
zhij <jizj2015@gmail.com>
2018-09-26 17:07:39 +0800

    修改文案

--- src/zh/js/script.js
+++ src/zh/js/script.js
@@ -7,47 +7,64 @@ $("#js-go-tree").click(function() {
 	$("html,body").animate({scrollTop: $("#js-tree").offset().top},500)
 })
 
-var treeData = {
-	yun: {
-		name: '云之树',
-		total: 8,
-		stock: 7
-	},
-	tan: {
-		name: '潭之树',
-		total: 8,
-		stock: 0
-	},
-	yan: {
-		name: '炎之树',
-		total: 8,
-		stock: 8
-	},
-	lei: {
-		name: '雷之树',
-		total: 8,
-		stock: 8
-	},
-	di: {
-		name: '地之树',
-		total: 8,
-		stock: 8
-	},
-	mai: {
-		name: '脉之树',
-		total: 8,
-		stock: 8
-	},
-	liu: {
-		name: '流之树',
-		total: 8,
-		stock: 8
-	},
-	feng: {
-		name: '风之树',
-		total: 8,
-		stock: 8
-	}
+var treeData = null
+
+function getTreeData(callback) {
+	// todo 请求各种树的库存
+	// $.ajax({
+		// url: '',
+		// success: function(res) {
+		// 	treeData = res.data.obj
+		// 	initTree();
+		// callback()
+		// }
+	// })
+	setTimeout(function() {
+		treeData = {
+			yun: {
+				name: '云之树',
+				total: 8,
+				stock: 7
+			},
+			tan: {
+				name: '潭之树',
+				total: 8,
+				stock: 0
+			},
+			yan: {
+				name: '炎之树',
+				total: 8,
+				stock: 8
+			},
+			lei: {
+				name: '雷之树',
+				total: 8,
+				stock: 8
+			},
+			di: {
+				name: '地之树',
+				total: 8,
+				stock: 8
+			},
+			mai: {
+				name: '脉之树',
+				total: 8,
+				stock: 8
+			},
+			liu: {
+				name: '流之树',
+				total: 8,
+				stock: 8
+			},
+			feng: {
+				name: '风之树',
+				total: 8,
+				stock: 8
+			}
+		}
+		initTree();
+		callback()
+	}, 2000)
 }
 $("#js-tree li").click(function() {
 	var name = $(this).attr("name");
@@ -57,13 +74,14 @@ $("#js-tree li").click(function() {
 	var treeObj = treeData[name]
 	var descHtml = '';
 	var sellOut = treeObj.total - treeObj.stock
-
+	descHtml = '已售' + sellOut + '棵，剩余' + treeObj.stock+ '棵';
 	if(treeObj.stock == 0) {
-		descHtml = '已售罄'
 		$('#js-btn-buy').hide();
+		$('#js-btn-sellout').show();
 	} else {
-		 descHtml = '已售' + sellOut + '棵，剩余' + treeObj.stock+ '棵';
+		
 		$('#js-btn-buy').show();
+		$('#js-btn-sellout').hide();
 	}
 	
 	$("#js-tree-status").text(descHtml)
@@ -84,5 +102,9 @@ function initTree() {
 		$(list[i]).children(".desc").text(desc)
 	}
 }
-initTree();
-$("#js-tree li").eq(0).click();
+
+getTreeData(function() {
+	$("#js-tree li").eq(0).click();
+})
+
+
